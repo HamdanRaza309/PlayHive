0e02a9a7411fb9a498953745121f8466c211add5
HamdanRaza309 <githubuse309@gmail.com>
2024-12-25 13:00:02 +0500

    The project setup is nearly complete.

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -10,12 +10,5 @@ const storage = multer.diskStorage({
 });
 
 export const upload = multer({
-    storage: storage,
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype.startsWith('image/')) {
-            cb(null, true);
-        } else {
-            cb(new Error('Only image files are allowed!'), false);
-        }
-    },
+    storage,
 });
