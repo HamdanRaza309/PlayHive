f44342ba822c69c1bb974ef7f7163cc0b75dda5e
HamdanRaza309 <githubuse309@gmail.com>
2025-05-02 18:33:02 +0500

    Add API to fetch all videos uploaded by the logged-in channel with owner details

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,6 +5,7 @@ import { Like } from "../models/like.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { populate } from "dotenv"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
@@ -12,8 +13,23 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 // TODO: Get all the videos uploaded by the channel
 const getChannelVideos = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
 
-})
+    if (!userId) {
+        throw new ApiError(400, 'User must be logged-in');
+    }
+
+    const loggedInUserVideos = await Video.find({ owner: userId }).populate('owner', 'fullname avatar');
+
+    if (!loggedInUserVideos || loggedInUserVideos.length === 0) {
+        throw new ApiError(404, 'No videos found for this channel');
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, loggedInUserVideos, 'Channel videos retrieved successfully'));
+});
+
 
 export {
     getChannelStats,
