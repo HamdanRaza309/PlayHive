5e853d8adcff414a0792f51e7318f17051caaee2
HamdanRaza309 <githubuse309@gmail.com>
2025-05-02 20:06:16 +0500

    Add detailed channel stats including subscribers, subscriptions, videos, likes, and views

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,9 +7,79 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { populate } from "dotenv"
 
+// TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+    const userId = req.user._id;
+
+    if (!userId) {
+        throw new ApiError(400, 'You must be logged in');
+    }
+
+    // Count of subscribers
+    const channelSubscribers = await Subscription.countDocuments({ channel: userId });
+
+    // Count of channels the user is subscribed to
+    const channelSubscribedTo = await Subscription.countDocuments({ subscriber: userId });
+
+    // Count of videos uploaded by the channel
+    const channelTotalVideos = await Video.countDocuments({ owner: userId });
+
+    // Total likes on all videos
+    const likesAggregation = await Like.aggregate([
+        {
+            $lookup: {
+                from: 'videos',
+                localField: 'video',
+                foreignField: '_id',
+                as: 'videoDetails'
+            }
+        },
+        { $unwind: '$videoDetails' },
+        {
+            $match: {
+                'videoDetails.owner': new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalLikes: { $sum: 1 }
+            }
+        }
+    ]);
+
+    const channelTotalLikesOnAllVideos = likesAggregation[0]?.totalLikes || 0;
+
+    // Total views on all videos
+    const viewsAggregation = await Video.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                totalViews: { $sum: '$views' }
+            }
+        }
+    ]);
+
+    const channelTotalViewsOnAllVideos = viewsAggregation[0]?.totalViews || 0;
+
+
+    const channelStats = {
+        channelSubscribers,
+        channelSubscribedTo,
+        channelTotalVideos,
+        channelTotalLikesOnAllVideos,
+        channelTotalViewsOnAllVideos
+    };
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channelStats, 'Channel stats retrieved successfully'));
+});
 
 // TODO: Get all the videos uploaded by the channel
 const getChannelVideos = asyncHandler(async (req, res) => {
